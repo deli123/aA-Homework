5ab5fe5a3aae18bf0f2906493fa4b19c7c66eae5
Derek <liderek63@gmail.com>
2022-06-28 21:58:03 -0700

    completed

---
+++ W3D3/recursion.rb
@@ -0,0 +1,81 @@
+def sum_to(n)
+    if n == 0
+        return 0
+    end
+    if n < 0
+        return nil
+    end
+    return n + sum_to(n - 1)
+end
+
+# Test Cases
+# p sum_to(5)  # => returns 15
+# p sum_to(1)  # => returns 1
+# p sum_to(9)  # => returns 45
+# p sum_to(-8)  # => returns nil
+
+
+def add_numbers(nums_array)
+    if nums_array.length == 1
+        return nums_array[0]
+    end
+    if nums_array == []
+        return nil
+    end
+
+    return nums_array.pop + add_numbers(nums_array)
+end
+
+# Test Cases
+# p add_numbers([1,2,3,4]) # => returns 10
+# p add_numbers([3]) # => returns 3
+# p add_numbers([-80,34,7]) # => returns -39
+# p add_numbers([]) # => returns nil
+
+def gamma_fnc(n)
+    if n == 0
+        return nil
+    end
+    if n == 1
+        return 1
+    end
+    return (n - 1) * gamma_fnc(n - 1)
+end
+
+# Test Cases
+# p gamma_fnc(0)  # => returns nil
+# p gamma_fnc(1)  # => returns 1
+# p gamma_fnc(4)  # => returns 6
+# p gamma_fnc(8)  # => returns 5040
+
+def ice_cream_shop(flavors, favorite)
+    if flavors.length == 0
+        return false
+    end
+    if flavors.pop == favorite
+        return true
+    end
+    return ice_cream_shop(flavors, favorite)
+end
+
+# Test Cases
+# p ice_cream_shop(['vanilla', 'strawberry'], 'blue moon')  # => returns false
+# p ice_cream_shop(['pistachio', 'green tea', 'chocolate', 'mint chip'], 'green tea')  # => returns true
+# p ice_cream_shop(['cookies n cream', 'blue moon', 'superman', 'honey lavender', 'sea salt caramel'], 'pistachio')  # => returns false
+# p ice_cream_shop(['moose tracks'], 'moose tracks')  # => returns true
+# p ice_cream_shop([], 'honey lavender')  # => returns false
+
+def reverse(string)
+    if string == ""
+        return ""
+    end
+    return string.slice!(-1) + reverse(string)
+end
+
+# Test Cases
+# p reverse("house") # => "esuoh"
+# p reverse("dog") # => "god"
+# p reverse("atom") # => "mota"
+# p reverse("q") # => "q"
+# p reverse("id") # => "di"
+# p reverse("") # => ""
