743ef9c250f489614a8a24e5d5a7f19da427c411
Derek <liderek63@gmail.com>
2022-07-05 20:46:00 -0700

    simon.rb complete

--- W4D3/simon/lib/simon.rb
+++ W4D3/simon/lib/simon.rb
@@ -4,19 +4,28 @@ class Simon
   attr_accessor :sequence_length, :game_over, :seq
 
   def initialize
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 
   def play
-
+    while !game_over
+      take_turn
+    end
+    game_over_message
+    reset_game
   end
 
   def take_turn
-
+    show_sequence
+    require_sequence
+    round_success_message if !game_over
+    @sequence_length += 1
   end
 
   def show_sequence
-
+    add_random_color
   end
 
   def require_sequence
@@ -24,18 +33,20 @@ class Simon
   end
 
   def add_random_color
-
+    @seq << COLORS.sample
   end
 
   def round_success_message
-
+    puts "Successful Round!"
   end
 
   def game_over_message
-
+    puts "Game Over!"
   end
 
   def reset_game
-
+    @sequence_length = 1
+    @game_over = false
+    @seq.clear
   end
 end
